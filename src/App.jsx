42535e395173fa424a227bb2f9f40997f144429c
Nyi Nyi Moe Htet <68096773+nyinyi2714@users.noreply.github.com>
2023-07-20 16:16:07 -0700

    Extracted StayForm Component from Rooms Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import SignIn from "./pages/SignIn/SignIn";
@@ -9,19 +9,29 @@ import ResetPassword from "./pages/ResetPassword/ResetPassword";
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 import "./App.css";
 
-function App() {
+export default function App() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [guestNum, setGuestNum] = useState(1);
+
+  const states = {
+    startDate,
+    setStartDate,
+    endDate,
+    setEndDate,
+    guestNum,
+    setGuestNum,
+  };
   return (
     <div className="app">
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/rooms" element={<RoomsPage />}  />
+        <Route path="/rooms" element={<RoomsPage states={states} />}  />
         <Route path="/resetpassword" element={<ResetPassword />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </div>
   );
 }
-
-export default App;
