f6eda9d035dafbf3122a54ca6c683f7fa5e512f6
raneenkakar <raneenkhan.12@gmail.com>
2023-07-24 19:26:57 -0700

    homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "./HomePage.css";
 
+
 export default function HomePage() {
+    
     return(
-        <div>HomePage</div>
+    <div id="sunsetImage">
+       <div class="container">
+           <div class="text_content">
+            <h1><span class="head_text">Hotel Del Luna</span> by Emerald</h1>
+            <p class="para">Find Deals for any season</p>
+            <p class="para">From cozy bed & breakfast to luxury hotels</p>
+            <a class="main_button" href ="book.now">Book Now</a>
+           </div> 
+       </div>
+       <section id="main_info">
+         
+       </section>
+   </div>
     );
 }
