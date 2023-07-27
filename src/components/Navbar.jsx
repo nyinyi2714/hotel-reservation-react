ceb1859afac77351a53164d0680d6a6af90528f5
raneenkakar <raneenkhan.12@gmail.com>
2023-07-26 18:15:18 -0700

    navbar SignIn and SignOut

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,19 +5,19 @@ export default function Navbar() {
 
     return(
         <nav class="nav">
-         <div class="navbar">
-            <div class="moto"><a href="#">Hotel Del Luna</a>
+           <div class="navbar">
+               <div class="moto"><a href="#">Hotel Del Luna</a>
+               </div>
+              <ul>
+                  <li><a href="#home">Home</a></li>
+                  <li><a href="#rooms">Rooms</a></li>
+                  <li><a href="#Location">Location</a></li>
+                  <li><a href="#ManageYourReservations">Reservation</a></li>
+                  <li><a href="#Manager">Manager</a></li>
+                  <a class="main_button" href ="Signout">Sign Out</a>
+                  <a class="main_button" href ="SignIn">Sign In</a>
+              </ul>
             </div>
-            <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#rooms">Rooms</a></li>
-                <li><a href="#ManageYourReservations">Manage Your Reservations</a></li>
-                <li><a href="#Location">Location</a></li>
-                <li><a href="#Manager">Manager</a></li>
-                <button><a href="#Signin">Sign In</a></button>
-                <button><a href="#Signout">Sign Out</a></button>
-            </ul>
-         </div>
-      </nav> 
+        </nav> 
     );
 }  
