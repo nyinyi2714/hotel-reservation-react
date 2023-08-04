5074146097d330c162fd9f1a97d529c54f56bcc3
Nyi Nyi Moe Htet <68096773+nyinyi2714@users.noreply.github.com>
2023-08-04 00:02:42 -0700

    Added cost column to reservation display

--- src/components/EditReservation/EditReservation.jsx
+++ src/components/EditReservation/EditReservation.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import StayForm from "../StayForm/StayForm";
 import "./EditReservation.css";
 
 export default function ManageReservation(props) {
+  const { closeEditModal } = props;
+  
   return (
     <div className="edit-reservation-modal">
-      {/* <StayForm states={props.states} /> */}
+      
     </div>
   );
 }
