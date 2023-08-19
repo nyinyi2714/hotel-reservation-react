84f37d665c65603abc6c0be847c144bbc32af13b
raneenkakar <raneenkhan.12@gmail.com>
2023-08-19 07:54:51 -0700

    added date and updated table

--- src/pages/Manager/Manager.jsx
+++ src/pages/Manager/Manager.jsx
@@ -1,68 +1,52 @@
 import React, { useState } from "react";
 import "./Manager.css";
+import DatePicker from "react-datepicker";
 import ReportContainer from "../../components/ReportContainer/ReportContainer";
 
 export default function Manager() {
-  
-  const [containerReports, setContainerReports] = useState ([
-    { id: 1, title: 'Report-1', date: '08-08-2023' },
-    { id: 2, title: 'Report-2', date: '08-08-2023' },
-  ]);
- 
-  const [data, setData] = useState('');
-  const [reports, setReports] = useState(containerReports);
+    
+  const [selectMonthYear, setSelectMonthYear] = useState(null);
 
-  const filterSearch = (e) => {
-    const searchValue = e.target.value.toLowerCase();
-    setData(searchValue);
-
-    if (searchValue !== '') {
-      const filteredReports = containerReports.filter((report) =>
-        Object.values(report).some((val) => String(val).toLowerCase().includes(searchValue))
-      );
-      setReports(filteredReports);
-    } else {
-      setReports(containerReports);
-    }
+  const handleMonthYear = (date) => {
+    setSelectMonthYear(date);
   };
+  
 
     return (
     <div className="manager__menu">
       <div className="manager__bar">
         <h2 className="manager__heading">Manager</h2>
         <ul>
-          <li><button className="manager__side__bar">Dashboard</button></li>
           <li><button className="manager__side__bar">Report</button></li>
         </ul>
       </div>
       <div>
-        <div className="manager__search">
-            <input 
-            className="manager__input" 
-            type="text" 
-            placeholder="Search..."
-            onChange={filterSearch}
-            />
-        </div>
         <div class="manager__new__report">
-          <button className="manager__new__button">New Reports</button>
-        </div>
+          <button className="manager__new__button">Generate Reports</button>
+          <DatePicker  className="manager__new__button"
+            selected={selectMonthYear}
+            onChange={handleMonthYear}
+            dateFormat="MMMM yyyy"
+            showMonthYearPicker
+          />
+       </div>
         <table>
           <thead>
             <tr>
               <th>ID</th>
-              <th>Title</th>
-              <th>Date</th>
+              <th>Profit</th>
+              <th>Booked</th>
+              <th>Cancellations</th>
             </tr>
           </thead>
           <tbody clasName="manager__table">
-            {reports.map(report => ( 
+            {/*reports.map(report => ( 
               <ReportContainer 
               key={report.id}
               report={report}
              />
             ))
-            }
+            */}
           </tbody>
         </table>
       </div>
